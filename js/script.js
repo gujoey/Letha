7ff8937dcbc527a348a963eb2f0c826617af47b2
Øyvind Gujord <oyvind_gujord@hotmail.com>
2018-05-13 13:59:07 +0200

    redesign

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function onLoad(){
-	/*if (window.innerWidth <= 768){
-		document.getElementById("imageIndex").src = "../images/photos/brogues-large.jpg";
-	}*/
+	if (window.innerWidth < 992){
+		document.getElementById("row-banner").innerHTML = "";
+	}
 }
 
 
@@ -61,4 +61,88 @@ function checkInputs(){
 		commentErr.style.display = "none";
 		document.getElementById("comment").style.borderColor = "green";
 	}
+}
+
+function checkInputsBuy(){
+	let email, emailErr, subject, subjectErr, comment, commentErr;
+	let regExEmail, name, nameErr, adress, adressErr, zip, zipErr;
+	let city, cityErr, country, countryErr;
+	
+	//check for input value in name field
+	name = document.getElementById("name").value;
+	nameErr = document.getElementById("nameError");
+	
+	if (name === "" || name === undefined){
+		nameErr.style.display = "block";
+		document.getElementById("name").style.borderColor = "red";
+		return false;
+	}else{
+		nameErr.style.display = "none";
+		document.getElementById("name").style.borderColor = "green";
+	}
+	
+	//check for valid input value in email field
+	regExEmail = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,4}$/;
+	email = document.getElementById("email").value;
+	emailErr = document.getElementById("emailError");
+	
+	if (!regExEmail.test(email)){
+		emailErr.style.display = "block";
+		document.getElementById("email").style.borderColor = "red";
+		return false;
+	}else{
+		emailErr.style.display = "none";
+		document.getElementById("email").style.borderColor = "green";
+	}
+	
+	//check for input value in adress field
+	adress = document.getElementById("adress").value;
+	adressErr = document.getElementById("adressError");
+	
+	if (adress === "" || adress === undefined){
+		adressErr.style.display = "block";
+		document.getElementById("adress").style.borderColor = "red";
+		return false;
+	}else{
+		adressErr.style.display = "none";
+		document.getElementById("adress").style.borderColor = "green";
+	}
+	
+	//check for input value in city field
+	zip = document.getElementById("zip").value;
+	zipErr = document.getElementById("zipError");
+	
+	if (zip === "" || zip === undefined){
+		zipErr.style.display = "block";
+		document.getElementById("zip").style.borderColor = "red";
+		return false;
+	}else{
+		zipErr.style.display = "none";
+		document.getElementById("zip").style.borderColor = "green";
+	}
+	
+	city = document.getElementById("city").value;
+	cityErr = document.getElementById("cityError");
+	
+	if (city === "" || city === undefined){
+		cityErr.style.display = "block";
+		document.getElementById("city").style.borderColor = "red";
+		return false;
+	}else{
+		cityErr.style.display = "none";
+		document.getElementById("city").style.borderColor = "green";
+	}
+	
+	country = document.getElementById("country").value;
+	countryErr = document.getElementById("countryError");
+	
+	if (country === "" || country === undefined){
+		countryErr.style.display = "block";
+		document.getElementById("country").style.borderColor = "red";
+		return false;
+	}else{
+		countryErr.style.display = "none";
+		document.getElementById("country").style.borderColor = "green";
+	}
+	
 }
